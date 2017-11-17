5b93600a06585a7888fefcfab231cdf57f95e6f9
Mark Eastwood <mark@markeastwood.net>
2017-11-17 10:08:10 +0000

    Added internal example class

---
+++ Slacker/Example.cs
@@ -0,0 +1,13 @@
+﻿namespace Slacker
+{
+    internal class Example
+    {
+        public Example()
+        {
+            ISlackConnectionProvider connectionProvider = new SlackConnectionProvider("Slack_WebHook_Url_With_Key_Here");
+            ISlackMessageWriter messageWriter = new SlackMessageWriter(connectionProvider);
+
+            messageWriter.Write("Channel Name", "Slacker", "Wow, we just sent a message to Slack using Slacker!");
+        }
+    }
+}
